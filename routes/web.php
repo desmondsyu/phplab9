ae823ab4fa46246aba7bc9ce9508dfa43071865f
Desmond <desmondzkx@gmail.com>
2024-11-11 21:41:02 -0500

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
-
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
@@ -10,14 +12,31 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
+// Default route: custom home page
 Route::get('/', function () {
     return view('custom_home');
 })->name('home');
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
-Route::post('/register', [AuthenticatedSessionController::class, 'store'])->name('register');
+// Authentication routes
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+Route::get('/register', [AuthenticatedSessionController::class, 'create'])->name('register'); // Example for registration
+
+// Protected routes (auth middleware)
+Route::middleware(['auth'])->group(function () {
+    // Inventory routes
+    Route::resource('inventory', InventoryController::class);
+
+    // Category routes
+    Route::resource('categories', CategoryController::class);
+
+    // Supplier routes
+    Route::resource('suppliers', SupplierController::class);
+});
+
